11f18c585d54bac97edec3aed4f26e4361e794f4
international-schools <101781503+Alancardosodemello@users.noreply.github.com>
2026-04-13 17:51:44 -0600

    Remove modal open/close event listeners

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-document.querySelectorAll('.open-modal').forEach(btn => {
-  btn.addEventListener('click', () => {
-    document.getElementById(btn.dataset.target).style.display = 'block';
-  });
-});
 
-document.querySelectorAll('.close').forEach(btn => {
-  btn.addEventListener('click', () => {
-    btn.closest('.modal').style.display = 'none';
-  });
-});
